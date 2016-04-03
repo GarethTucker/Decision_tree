27d5f85abcc17279be77b738b2a10821c40cc789
Gareth Tucker <Gareth Tucker>
2016-04-03 22:15:49 +1200

    Prints to file

--- Main.py
+++ Main.py
@@ -15,6 +15,14 @@ class Main(object):
             self.training_list = [x for x in self.training_list if x != []]
             self.training_list[1] = ['RESULT'] + self.training_list[1]
 
+def report(indent, node):
+        if not node.true and not node.false:
+            output_file.write('{0} Class: {1}, prob = {2}\n'.format(indent, node.attribute, node.probability))
+        else:
+            output_file.write('{0}{1} = TRUE:\n'.format(indent, node.attribute))
+            report(('{0}    '.format(indent)), node.true)
+            output_file.write('{0}{1} = FALSE:\n'.format(indent, node.attribute))
+            report(('{0}    '.format(indent)), node.false)
 
 if __name__=="__main__":
     data = Main('data/hepatitis-test.dat', 'data/hepatitis-training.dat')
@@ -22,9 +30,29 @@ if __name__=="__main__":
     #                  for row in data.training_list]))
     t = Tree(data)
 
+    output_file = open('../decision_tree_classification.txt', 'w')
+
     tree = t.tree
-    tree.report('')
-    print('Classifying test data...')
+    report('', tree)
+    output_file.write('Classifying test data...\n')
     c = Classify(data, tree)
-    print('Num correct: {0}, Num tested: {1}'.format(c.number_correct, c.number_tested))
-    print('Test showed accuracy of {0}'.format(float(c.number_correct)/c.number_tested))
+    output_file.write('Num correct: {0}, Num tested: {1}\n'.format(c.number_correct, c.number_tested))
+    output_file.write('Test showed accuracy of {0}\n'.format(float(c.number_correct)/c.number_tested))
+
+    output_file.write('\nClassifying data split into 10...\n')
+    total_accuracy_of_ten_runs = 0
+    for i in range(1, 11):
+        test_file_name = 'data/hepatitis-test-run'
+        training_file_name = 'data/hepatitis-training-run'
+        i = str(0)+str(i) if i < 10 else str(i)
+        test_file_name = test_file_name+i+'.dat'
+        training_file_name = training_file_name+i+'.dat'
+        data = Main(test_file_name, training_file_name)
+        t=Tree(data)
+        tree = t.tree
+        c = Classify(data, tree)
+        run_accuracy = float(c.number_correct)/c.number_tested
+        output_file.write('run {0} accuracy: {1}\n'.format(i, run_accuracy))
+        total_accuracy_of_ten_runs += run_accuracy
+    average_accuracy_of_ten_runs = total_accuracy_of_ten_runs/10
+    output_file.write('average accuracy of ten runs: {0}\n'.format(average_accuracy_of_ten_runs))
--- Node.py
+++ Node.py
@@ -5,12 +5,3 @@ class Node (object):
         self.true = true
         self.false = false
         self.probability = probability
-
-    def report(self, indent):
-        if not self.true and not self.false:
-            print('{0} Class: {1}, prob = {2}'.format(indent, self.attribute, self.probability))
-        else:
-            print('{0}{1} = TRUE:'.format(indent, self.attribute))
-            self.true.report('{0}    '.format(indent))
-            print('{0}{1} = FALSE:'.format(indent, self.attribute))
-            self.false.report('{0}    '.format(indent))
